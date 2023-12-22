dbe5578ca5fa193a8de76bf9dae0f76c3a6c6f37
unknown <aslan.thr@gmail.com>
2023-12-22 15:51:05 -0500

    Added event listeners to the canvas

---
+++ index.js
@@ -0,0 +1,30 @@
+var stage = new createjs.Stage("myStage");
+
+stage.enableDOMEvents(true);
+//var drawingCanvas = new createjs.Shape();
+var drawingCanvas = new createjs.Container();
+//stage.addChild(container);
+stage.addEventListener("stagemousedown", drawStart);
+stage.addEventListener("stagemouseup", drawEnd);
+
+stage.addChild(drawingCanvas);
+//stage.update();
+createjs.Ticker.addEventListener("tick", tick);
+var color,
+  stroke,
+  oldPt,
+  oldMidPt,
+  midPt,
+  txt,
+  circle,
+  toUpdate = true,
+  newCircle = true;
+function drawStart() {
+  color = "#FEDCBA";
+  stroke = 1;
+  oldPt = createPoint();
+  oldMidPt = oldPt;
+
+  stage.addEventListener("stagemousemove", drawMovement);
+  toUpdate = true;
+}
