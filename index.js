b91e7f09224433e94120f17294cab2020c2c66bf
unknown <aslan.thr@gmail.com>
2023-12-22 15:58:33 -0500

    Added function to create a new circle, following mouse click

--- index.js
+++ index.js
@@ -31,3 +31,34 @@ function drawStart() {
 function createPoint(x, y) {
   return new createjs.Point(x || stage.mouseX, y || stage.mouseY);
 }
+function drawMovement() {
+  var pt = createPoint();
+  var radius = Math.sqrt(
+    Math.pow(oldMidPt.x - pt.x, 2) + Math.pow(oldMidPt.y - pt.y, 2)
+  );
+  if (!newCircle) {
+    circle && drawingCanvas.removeChild(circle);
+    txt && drawingCanvas.removeChild(txt);
+  }
+  circle = new createjs.Shape();
+  circle.graphics
+    .setStrokeStyle(stroke, "round", "round")
+    .beginStroke(color)
+    .moveTo(oldMidPt.x, oldMidPt.y)
+    .lineTo(pt.x, pt.y)
+    .moveTo(oldMidPt.x, oldMidPt.y)
+    .beginFill("rgba(255,255,255,0.6)")
+    .drawCircle(oldMidPt.x, oldMidPt.y, radius);
+  drawingCanvas.addChild(circle);
+
+  //  console.log(radius);
+  oldPt = pt;
+  txt = new createjs.Text((radius | 0) + "", "10px Arial", "#FFF");
+  txt.x = (pt.x + oldMidPt.x) >> 1;
+  txt.y = (pt.y + oldMidPt.y) >> 1;
+  //  console.log(txt);
+  drawingCanvas.addChild(txt);
+  // stage.update();
+  toUpdate = true;
+  newCircle = false;
+}
